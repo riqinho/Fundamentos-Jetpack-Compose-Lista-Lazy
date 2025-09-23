077f9de03e8bb77bad96ba2a7398ae1cd6c0f0bd
Rick Alves Domingues <rickpoterdom@gmail.com>
2025-09-22 21:46:35 -0300

    implementacao da lazycolumn

--- app/src/main/java/riqinho/com/github/fundamentos_jetpack_compose_listas_lazy/MainActivity.kt
+++ app/src/main/java/riqinho/com/github/fundamentos_jetpack_compose_listas_lazy/MainActivity.kt
@@ -10,6 +10,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Icon
@@ -23,6 +25,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import riqinho.com.github.fundamentos_jetpack_compose_listas_lazy.repository.getAllGames
 import riqinho.com.github.fundamentos_jetpack_compose_listas_lazy.ui.theme.FundamentosjetpackcomposelistaslazyTheme
 
 class MainActivity : ComponentActivity() {
@@ -65,5 +68,12 @@ fun GamesScreen(modifier: Modifier = Modifier) {
             }
         )
         Spacer(modifier = Modifier.height(16.dp))
+        LazyColumn() {
+            items(getAllGames()) {
+                Column {
+                    Text(text = it.title)
+                }
+            }
+        }
     }
 }
